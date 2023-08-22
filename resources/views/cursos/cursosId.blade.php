aba40e5684cebbf4fc9ec7b573527d31797aaba3
AnderTorne <aatorne@outlook.com>
2023-08-22 01:29:32 -0600

    Imagenes video

--- resources/views/cursos/cursosId.blade.php
+++ resources/views/cursos/cursosId.blade.php
@@ -5,8 +5,8 @@
         <a href="{{route('cursos.edit',$curso)}}" class="btn btn-primary mb-2 position-absolute end-0"> Editar curso</a>
         <br>
         <br>
-        <title>{{$curso->nombre_curso}}</title>
-        <h1>Cursos {{$curso->nombre_curso}} </h1>
+        {{-- <title>{{$curso->nombre_curso}}</title>
+        <h1>Curso: {{$curso->nombre_curso}} </h1>
         <p>Categoria: {{$curso->categoria_curso}}</p>
         <p>Descripcion: {{$curso->descripcion_curso}}</p>
         <p>Precio: {{$curso->precio_curso}}</p>
@@ -16,7 +16,30 @@
             title="YouTube video player" frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             allowfullscreen>
-        </iframe>
+        </iframe> --}}
+
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card mb-4"  >
+                        <div class="card-body">
+                            <iframe 
+                                width="1024" height="576" src="https://www.youtube.com/embed/{{$curso->url_video_curso}}" 
+                                title="YouTube video player" frameborder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                                allowfullscreen>
+                            </iframe>
+                        
+                            <h2 class="card-title">Curso: {{$curso->nombre_curso}}</h2>
+                            <h3 class="card-text">Categoria: {{$curso->descripcion_curso}}</h3>
+                            <h3 class="card-text">Precio: {{$curso->precio_curso}}</h3>
+                            <h3 class="card-text">Valoracion: {{$curso->calificacion_curso}}/5</h3>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
 
     </x-slot>
 
